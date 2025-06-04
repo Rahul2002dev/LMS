b9b19f998aebe11e12444b1f7278392601387875
rahul Patel <rahul26patel2002@gmail.com>
2025-06-04 11:02:21 +0530

    [rahulPatel add mutual-fund-tracker updated

--- course/src/main/java/org/example/course/CourseApplication.java
+++ course/src/main/java/org/example/course/CourseApplication.java
@@ -1,9 +1,12 @@
 package org.example.course;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CourseApplication {
 
     public static void main(String[] args) {
